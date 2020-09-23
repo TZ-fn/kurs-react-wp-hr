047cdd4aed3c01f1c6a6eff345a994b487847517
Tomek <tomekz@vp.pl>
2020-09-23 15:55:31 +0200

    Add Button component, add GlobalStyle component

--- src/views/Root/Root.jsx
+++ src/views/Root/Root.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
+import Button from 'components/Button/Button';
+import GlobalStyle from 'theme/GlobalStyle';
 
-const Root = () => <h1>Hello world!</h1>;
+const Root = () => (
+  <>
+    <GlobalStyle />
+    <h1>Hello world!</h1>
+    <Button>Close / Save</Button>
+    <Button secondary>Remove</Button>
+  </>
+);
 
 export default Root;
