e7deb3cec1a76a6f042aed392914a905459e0714
Tomek <tomekz@vp.pl>
2020-09-25 15:57:01 +0200

    Add ThemeProvider, add Inpunt component

---
+++ src/theme/mainTheme.js
@@ -0,0 +1,19 @@
+const theme = {
+  primary: 'hsl(49,100%,58%)',
+  secondary: 'hsl(196,83%,75%)',
+  tertiary: 'hsl(106,47%,64%)',
+  gray100: 'hsl(0,0%,96%)',
+  gray200: 'hsl(0,0%,90%)',
+  gray300: 'hsl(0, 0%, 80%)',
+  black: 'hsl(0,0%,0%)',
+  light: 300,
+  regular: 500,
+  bold: 600,
+  fontSize: {
+    s: '12px',
+    m: '16px',
+    l: '20px',
+  },
+};
+
+export default theme;
--- src/views/Root/Root.jsx
+++ src/views/Root/Root.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import Button from 'components/Button/Button';
+import Button from 'components/atoms/Button/Button';
+import { ThemeProvider } from 'styled-components';
 import GlobalStyle from 'theme/GlobalStyle';
+import { theme } from 'theme/mainTheme';
 
 const Root = () => (
   <>
     <GlobalStyle />
-    <h1>Hello world!</h1>
-    <Button>Close / Save</Button>
-    <Button secondary>Remove</Button>
+    <ThemeProvider theme={theme}>
+      <>
+        <h1>Hello world!</h1>
+        <Button>Close / Save</Button>
+        <Button secondary>Remove</Button>
+      </>
+    </ThemeProvider>
   </>
 );
 
