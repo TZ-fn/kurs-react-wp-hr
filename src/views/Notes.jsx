18e386bb17c5911f1c662b33db18aec9a80e54b3
Tomek <tomekz@vp.pl>
2020-10-07 14:06:26 +0200

    Add views components

---
+++ src/views/Notes.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Notes = () => <h2>Notes</h2>;
+
+export default Notes;
---
+++ src/views/Twitters.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Twitters = () => <h2>Twitters</h2>;
+
+export default Twitters;
