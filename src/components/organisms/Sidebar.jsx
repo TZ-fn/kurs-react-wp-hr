06c6877f9e9b2826438bb6ff52a1712cbfddfff3
Tomek <tomekz@vp.pl>
2020-10-09 15:54:33 +0200

    Add styling to the Sidebar

--- src/components/organisms/Sidebar.jsx
+++ src/components/organisms/Sidebar.jsx
@@ -1,21 +1,53 @@
 import React from 'react';
 import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 import ButtonIcon from 'components/atoms/ButtonIcon/ButtonIcon';
+import logoIcon from 'assets/icons/logo.svg';
 import BulbIcon from 'assets/icons/bulb.svg';
 import LogoutIcon from 'assets/icons/logout.svg';
 import PenIcon from 'assets/icons/pen.svg';
 import TwitterIcon from 'assets/icons/twitter.svg';
 
+const StyledWrapper = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 150px;
+  height: 100vh;
+  background-color: ${({ activeColor, theme }) => (activeColor ? theme[activeColor] : theme.note)};
+`;
+
+const StyledLogoLink = styled(NavLink)`
+  display: block;
+  width: 67px;
+  height: 67px;
+  background-image: url(${logoIcon});
+  background-repeat: no-repeat;
+  background-position: 50% 50%;
+  background-size: 80%;
+  border: none;
+  margin-bottom: 10vh;
+`;
+
+const StyledLogoutButton = styled(ButtonIcon)`
+  margin-top: auto;
+`;
+
+const StyledLinksList = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+`;
+
 const Sidebar = () => (
-  <div>
-    <p>logo</p>
-    <div>
-      <ButtonIcon icon={PenIcon} />
-      <ButtonIcon icon={TwitterIcon} />
-      <ButtonIcon icon={BulbIcon} />
-    </div>
-    <ButtonIcon icon={LogoutIcon} />
-  </div>
+  <StyledWrapper>
+    <StyledLogoLink to="/" />
+    <ButtonIcon as={NavLink} to="/" icon={PenIcon} />
+    <ButtonIcon as={NavLink} to="/twitters" icon={TwitterIcon} />
+    <ButtonIcon as={NavLink} to="/articles" icon={BulbIcon} />
+
+    <StyledLogoutButton as={NavLink} to="/" icon={LogoutIcon} />
+  </StyledWrapper>
 );
 
 export default Sidebar;
--- src/components/organisms/Sidebar.stories.jsx
+++ src/components/organisms/Sidebar.stories.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { withKnobs } from '@storybook/addon-knobs';
+import StoryRouter from 'storybook-react-router';
 import Sidebar from './Sidebar';
 
 export default {
   title: 'Design System/Organisms/Sidebar',
   component: Sidebar,
-  decorators: [withKnobs],
+  decorators: [withKnobs, StoryRouter()],
 };
 
 export const Normal = () => <Sidebar />;
