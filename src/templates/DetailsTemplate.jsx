db65aec799093d956b30b950246fdc1c4cd349a7
Tomek <tomekz@vp.pl>
2020-10-20 15:43:56 +0200

    Add functionality to Details pages

--- src/templates/DetailsTemplate.jsx
+++ src/templates/DetailsTemplate.jsx
@@ -1,19 +1,10 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
 import UserPageTemplate from 'templates/UserPageTemplate';
-import Heading from 'components/atoms/Heading/Heading';
-import Paragraph from 'components/atoms/Paragraph/Paragraph';
-import Button from 'components/atoms/Button/Button';
 
-const DetailsTemplate = () => (
-  <UserPageTemplate>
-    <h1>Note</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime id dolorum odit reprehenderit
-      repellendus nesciunt sapiente
-    </p>
+const DetailsTemplate = ({ children, pageType }) => (
+  <UserPageTemplate pageType={pageType}>
+    {children}
     <Link to="/">go back</Link>
   </UserPageTemplate>
 );
--- src/views/DetailsPage.jsx
+++ src/views/DetailsPage.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import DetailsTemplate from 'templates/DetailsTemplate';
+import routes from 'routes/index';
 
-const DetailsPage = () => (
-  <>
-    <DetailsTemplate />
-  </>
-);
+class DetailsPage extends Component {
+  state = {
+    pageType: 'notes',
+  };
+
+  componentDidMount() {
+    switch (this.props.match.path) {
+      case routes.note:
+        this.setState({ pageType: 'notes' });
+        break;
+      case routes.twitter:
+        this.setState({ pageType: 'twitters' });
+        break;
+      case routes.article:
+        this.setState({ pageType: 'articles' });
+        break;
+    }
+  }
+
+  render() {
+    const { pageType } = this.state;
+    return (
+      <DetailsTemplate pageType={pageType}>
+        <p>{pageType}</p>
+      </DetailsTemplate>
+    );
+  }
+}
 
 export default DetailsPage;
