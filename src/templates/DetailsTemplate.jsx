7fe8a7bdb2aa7ad98661d98e3b6847fd5127a328
Tomek <tomekz@vp.pl>
2020-10-21 15:50:34 +0200

    Add functionality to Details Page and Template

--- src/templates/DetailsTemplate.jsx
+++ src/templates/DetailsTemplate.jsx
@@ -1,11 +1,75 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 import UserPageTemplate from 'templates/UserPageTemplate';
+import Heading from 'components/atoms/Heading/Heading';
+import Paragraph from 'components/atoms/Paragraph/Paragraph';
+import Button from 'components/atoms/Button/Button';
 
-const DetailsTemplate = ({ children, pageType }) => (
+const StyledWrapper = styled.div`
+  padding: 25px 150px 25px 70px;
+`;
+
+const StyledCardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  height: 90vh;
+  max-width: 60vw;
+  padding: 25px 50px;
+  box-shadow: 0 10px 30px -10px hsla(0, 0%, 0%, 0.1);
+
+  @media (max-width: 1200px) {
+    max-width: 80vw;
+  }
+`;
+
+const StyledHeading = styled(Heading)`
+  margin-bottom: 10px;
+`;
+
+const StyledCreatedParagraph = styled(Paragraph)`
+  margin: 0;
+  font-size: ${({ theme }) => theme.fontSize.xs};
+  font-weight: ${({ theme }) => theme.bold};
+  text-transform: uppercase;
+`;
+const StyledContentParagraph = styled(Paragraph)`
+  margin: 40px 0 40px;
+`;
+
+const StyledLink = styled.a`
+  display: block;
+  font-weight: ${({ theme }) => theme.bold};
+  font-size: ${({ theme }) => theme.fontSize.xs};
+  color: black;
+  text-transform: uppercase;
+  margin: 20px 0 70px;
+`;
+
+const StyledDeleteButton = styled(Button)`
+  padding: 0;
+  margin: 0;
+  text-transform: none;
+  background: none;
+  border: none;
+  font-size: ${({ theme }) => theme.fontSize.xs};
+`;
+
+const DetailsTemplate = ({ pageType, title, created, content, articleUrl, twitterName }) => (
   <UserPageTemplate pageType={pageType}>
-    {children}
-    <Link to="/">go back</Link>
+    <StyledWrapper>
+      <StyledCardWrapper>
+        <StyledHeading big>{title}</StyledHeading>
+        <StyledCreatedParagraph>{created}</StyledCreatedParagraph>
+        <StyledContentParagraph>{content}</StyledContentParagraph>
+        {pageType === 'articles' && <StyledLink href={articleUrl}>Open this article</StyledLink>}
+        <Button as={Link} to={`/${pageType}`} pageType={pageType}>
+          Save / Close
+        </Button>
+        <StyledDeleteButton>remove note</StyledDeleteButton>
+      </StyledCardWrapper>
+    </StyledWrapper>
   </UserPageTemplate>
 );
 
--- src/views/DetailsPage.jsx
+++ src/views/DetailsPage.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import DetailsTemplate from 'templates/DetailsTemplate';
 import routes from 'routes/index';
 
@@ -22,13 +23,33 @@ class DetailsPage extends Component {
   }
 
   render() {
+    const dummyArticle = {
+      id: 1,
+      title: 'Wake me up when Vue ends',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur modi dolore voluptatum quia. Pariatur doloribus quibusdam repellendus harum recusandae minima qui soluta molestiae quisquam expedita, ea ducimus asperiores eum earum?',
+      twitterName: 'hello_roman',
+      articleUrl: 'https://youtube.com/helloroman',
+      created: '1 day',
+    };
+
     const { pageType } = this.state;
+
     return (
-      <DetailsTemplate pageType={pageType}>
-        <p>{pageType}</p>
-      </DetailsTemplate>
+      <DetailsTemplate
+        pageType={pageType}
+        title={dummyArticle.title}
+        created={dummyArticle.created}
+        content={dummyArticle.content}
+        twitterName={dummyArticle.twitterName}
+        articleUrl={dummyArticle.articleUrl}
+      />
     );
   }
 }
+
+DetailsPage.propTypes = {
+  match: PropTypes.object.isRequired,
+};
 
 export default DetailsPage;
