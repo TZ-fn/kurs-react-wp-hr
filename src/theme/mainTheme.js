f0a4e89ca3fa85841c0897139e15ccd11740a74a
Tomek <tomekz@vp.pl>
2020-11-18 15:36:31 +0100

    Add another font size to the theme file

--- src/theme/mainTheme.js
+++ src/theme/mainTheme.js
@@ -9,8 +9,9 @@ const theme = {
   light: 300,
   bold: 600,
   fontSize: {
-    xxs: '1rem',
-    xs: '1.2rem',
+    xxxs: '1rem',
+    xxs: '1.2rem',
+    xs: '1.4rem',
     s: '1.6rem',
     m: '2.1rem',
     l: '2.4rem',
